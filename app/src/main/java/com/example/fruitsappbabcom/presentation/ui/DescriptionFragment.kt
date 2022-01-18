5c55707fa8ba64158f4af8bcde01c1d4df799721
mixaefes <mixaefes@gmail.com>
2022-01-18 17:06:51 +0200

    feat: current item on description fragment

--- app/src/main/java/com/example/fruitsappbabcom/presentation/ui/DescriptionFragment.kt
+++ app/src/main/java/com/example/fruitsappbabcom/presentation/ui/DescriptionFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import coil.api.load
 import com.example.fruitsappbabcom.databinding.FragmentDescriptionBinding
+import com.example.fruitsappbabcom.presentation.viewModel.SharedViewModel
 
 
 class DescriptionFragment : Fragment() {
    private var _binding : FragmentDescriptionBinding? = null
     private  val binding get() = _binding!!
+    private val sharedViewModel : SharedViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +24,11 @@ class DescriptionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDescriptionBinding.inflate(inflater,container,false)
+        val selectedFruit = sharedViewModel.selectedItem.value
+        binding.imageViewDescription.load(selectedFruit?.image)
+        binding.textViewName.text = selectedFruit?.name
+        binding.textViewDescription.text = selectedFruit?.description
+        binding.textViewPrice.text = selectedFruit?.price.toString()
         return binding.root
     }
 
